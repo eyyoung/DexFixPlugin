a842e09a04883d709d8a6bef1a7a90af5a8cd4d8
Young Yang <young91@vip.qq.com>
2016-08-31 17:06:05 +0800

    For first version

---
+++ plugin/src/main/groovy/me/young/android/gradle/dexfix/DexFixExtention.groovy
@@ -0,0 +1,10 @@
+package me.young.android.gradle.dexfix;
+
+/**
+ * Created by Young on 2016/8/31.
+ */
+public class DexFixExtention {
+
+    def mainDexRules = "build/dexfix/mainDexClasses.rules";
+
+}
